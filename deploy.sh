b2cebb40def0717c05fbc6f56fdbd7f55cf60b2f
Oleg Safonov <doroxz@protonmail.com>
2018-10-07 04:39:43 +0300

    ci: give execute rights to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 if [ -n "$GITHUB_TOKEN" ]; then
-    cd "$TRAVIS_BUILD_DIR"
     curl -H "Authorization: token $GITHUB_TOKEN"
     git checkout master
     git branch -f -q https://<me>:$GITHUB_API_KEY@github.com/doroxz/test develop &>/dev/null
