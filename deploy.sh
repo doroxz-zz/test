d02b6b48dad0e15b518c0ebc88407b3a72f11e4a
Oleg Safonov <doroxz@protonmail.com>
2018-10-07 06:10:28 +0300

    fix: update script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 if [ -n "$GITHUB_TOKEN" ]; then
+    GITHUB_URL="$(git config --get remote.origin.url)"
+    # curl -H -q "Authorization: token ${GITHUB_TOKEN}" ${GITHUB_URL}
+    proto="$(echo $GITHUB_URL | grep :// | sed -e's,^\(.*://\).*,\1,g')"
+    url_parsed="$(echo ${GITHUB_URL/$proto/})"
+    url="${proto}doroxz:${GITHUB_TOKEN}@${url_parsed}"
     git checkout master
-    git branch -f -q https://doroxz:$GITHUB_TOKEN@github.com/doroxz/test develop
+    git pull origin
+    git push -f $url develop
 fi
